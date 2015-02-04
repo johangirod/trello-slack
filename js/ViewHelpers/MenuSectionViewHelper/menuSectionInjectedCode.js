ef4f31d0d3f1589da980d16a5e693c133de910ff
Yvan Wibaux <yvan@evaneos.com>
2015-02-04 12:15:11 +0100

    debug prefe

--- js/ViewHelpers/MenuSectionViewHelper/menuSectionInjectedCode.js
+++ js/ViewHelpers/MenuSectionViewHelper/menuSectionInjectedCode.js
@@ -38,3 +38,15 @@ $( "body" ).on( "click", ".SPM-no-slack", function() {
     alert($(this).nextAll('.SPM-error').attr('data-text'));
 
 });
+
+if ($("#sidebar_behavior").length > 0) {
+    $("body").addClass('SPM-not-show-all');
+}
+
+$("body").on('change', '#sidebar_behavior_select', function() {
+    if ($("#sidebar_behavior").length > 0) {
+        $("body").addClass('SPM-not-show-all');
+    } else {
+        $("body").removeClass('SPM-not-show-all');
+    }
+})
